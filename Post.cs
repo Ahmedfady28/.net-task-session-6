37a85f62813fcd27090929024e8d480778bb4389
Ahmedfady28 <153878057+Ahmedfady28@users.noreply.github.com>
2025-05-23 06:50:45 +0300

    Add stack overflow files

---
+++ Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    internal class Post : Votable
+    {
+        public string title;
+        public string desc;
+        public int voteCount { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public Post(string title, string desc)
+        {
+            this.title = title;
+            this.desc = desc;
+            voteCount = 0;
+            CreatedAt = DateTime.Now;
+        }
+
+        public Post()
+        {
+            title = "No title";
+            desc = "No description";
+            voteCount = 0;
+            CreatedAt = DateTime.Now;
+        }
+        public void UpVote()
+        {
+            voteCount++;
+        }
+        public void DownVote()
+        {
+            if (voteCount <= 0)
+            {
+                throw new InvalidOperationException("Vote count cannot be negative.");
+            }
+
+            voteCount--;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Title: {title}, Description: {desc}, Vote Count: {voteCount}, Created At: {CreatedAt}";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
-﻿namespace stopwatch
+﻿namespace StackOverflow
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            System.Threading.Thread.Sleep(5000);
-            stopwatch.Stop();
+            var post = new Post("My first post", "This is the description of my first post.");
+            var post2 = new Post();
 
+            post.UpVote();
+            post.UpVote();
 
-            stopwatch.Start();
-            System.Threading.Thread.Sleep(5000);
-            stopwatch.Stop();
+            post.DownVote();
+            post.DownVote();
+
+            post2.UpVote();
+            post2.UpVote();
+            post2.UpVote();
+
+            post2.DownVote();
+            post2.DownVote();
+
+            //post.DownVote(); // this line will throw an exception
+
+            Console.WriteLine(post.ToString());
+            Console.WriteLine("===================================");
+            Console.WriteLine(post2.ToString());
         }
     }
 }
